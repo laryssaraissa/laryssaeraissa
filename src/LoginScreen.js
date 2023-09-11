a99d4072ed8ab0d1b328a0984d7a29830859948d
Laryssa <laryssag.santos.1b1.etec2021@gmail.com>
2023-09-11 10:34:03 -0300

    branch com o código da aula do dia Mon, Sep 11, 2023 10:34:03 AM

---
+++ src/LoginScreen.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native-web";
+
+const LoginScreen = () => {
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+const handleLogin = () => {
+        console.log(`login com Email: ${email} e senha: ${password}`);
+};
+
+return(
+
+    <View>
+        <Text>Tela login</Text>
+    </View>
+
+);
+
+};
+export default LoginScreen;
+
